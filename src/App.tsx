32bf8a7ece2535378429a404bc19389165f96120
Kenneth Dave <dave.bahana@gmail.com>
2024-04-09 00:09:26 +0700

    refactor: cleaning some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 // import React, { useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
-import Homepage from './pages/Homepage'
 import Main from './pages/Main'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ParticleComponents from './animations/Particles'
-import Projects from './pages/Projects'
+
 function App() {
   return (
     <ChakraProvider>
@@ -13,10 +12,8 @@ function App() {
         <Navbar />
         <ParticleComponents />
           <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<Main />} />
             <Route path="/particles" element={<ParticleComponents />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/main" element={<Main />} />
           </Routes>
         
       </BrowserRouter>
